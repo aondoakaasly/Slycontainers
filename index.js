e59d04da535179a94b8a1eef4943f71fa5ef7c9e
shazaniyu55 <shazaniyu@gmail.com>
2024-12-26 15:28:40 +0100

    niyu

--- index.js
+++ index.js
@@ -304,9 +304,9 @@ app.get('/logout', (req, res) => {
 
 app.get('/checkout', (req, res)=>{})
 
-app.use((req, res, next) => {
-    res.status(404).render('404');
-});
+// app.use((req, res, next) => {
+//     res.status(404).render('404');
+// });
   
 //listening to the port
 app.listen(port, ()=>{
